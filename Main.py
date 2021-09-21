2d2e048bf7f30efaaecbd49c73f04c74deb4719f
vitorcontieri <vitor.pecanha@hotmail.com>
2021-09-21 19:01:00 -0300

    Criei a funcao sigmoidal

--- Main.py
+++ Main.py
@@ -2,3 +2,14 @@
 Método main do estudo de Redes Neurais.
 O objetivo desse projeto é desenvolver uma rede neural do zero.
 """
+
+import numpy as np
+
+def sigmoid(x):
+    """
+    Função sigmoidal. Retorna o valor de x aplicado na função sigmoid.
+    x: inteiro obrigatório
+    """
+    return 1 / (1 + np.exp(-x))
+
+print(sigmoid(7))
