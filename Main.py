fbfb5ce9657d1ef2f6c128661f1f08fd3b5434f9
vitorcontieri <vitor.pecanha@hotmail.com>
2021-09-21 21:31:34 -0300

    Criei a classe Neuron

--- Main.py
+++ Main.py
@@ -8,8 +8,39 @@ import numpy as np
 def sigmoid(x):
     """
     Função sigmoidal. Retorna o valor de x aplicado na função sigmoid.
-    x: inteiro obrigatório
+    x: 
+        Inteiro obrigatório.
+        Valor que será calculado pela função.
     """
     return 1 / (1 + np.exp(-x))
 
-print(sigmoid(7))
+class Neuron:
+    """
+    Classe que gera um neurônio para a rede neural
+    """
+    def __init__(self, weights, bias):
+        """
+        Classe que inicializa o neurônio.
+        weights:
+            Np.array.
+            Peso dos neurônios.
+        bias:
+            Inteiro.
+            Valor do enviesamento.
+        """
+        self.weights = weights
+        self.bias = bias
+    
+    def feedfoward(self, inputs, func=sigmoid()):
+        """
+        Função de alimentação do neurônio.
+        inputs:
+            Np.array.
+            Valor que será calculado.
+        func:
+            Função que será usada para os calculos.
+            Por padrão é a função sigmoid.
+        """
+        total = np.dot(self.weights, inputs) + self.bias
+        return func(total)
+
